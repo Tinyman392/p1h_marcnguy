537062d869cde847f3aba91c18116e2ba5b65215
Marcus Nguyen <tinyman392@gmail.com>
2017-10-11 09:53:34 -0500

    Updated download from PubChem script to parse search result page for first PC ID and attempt to download that

--- downloadPubChem/downloadPubChem.sh
+++ downloadPubChem/downloadPubChem.sh
@@ -18,20 +18,34 @@ url="https://www.ncbi.nlm.nih.gov/pccompound?term="$sTerm
 #
 # We want to get the final URL that pops out of the, I use wget
 # to do this with the --content-disposition option, I also send
-# the (useless) downloaded content to /dev/null
+# the (useless) downloaded content to temp.html.
 #
 # The error output is redirected to grep to find the word 
 # "Location" which contains the final URL, this is then cut to
 # parse out the URL and the PubChem ID
 #
-pcid=$(wget $url --content-disposition -O /dev/null 2> >(grep Location) | cut -f2 -d' ' | rev | cut -f1 -d'/' | rev)
+# Note that temp.html is only used if the URL brings us to a 
+# search results page (multiple compounds) in which case, we try
+# to extract the first search hit.  If none exist, we don't do
+# anything
+#
+pcid=$(wget $url --content-disposition -O temp.html 2> >(grep Location) | cut -f2 -d' ' | rev | cut -f1 -d'/' | rev)
 
 #
-# If a PubChem ID couldn't be found, exit the script
+# If a PubChem ID couldn't be found, grab first item in search 
+# results
+# If no results exist, exit script
 #
 if [ "$pcid" == "" ]; then
-	exit
+	if [[ $(grep "Select item" temp.html) =~ (Select item )([0-9]*) ]]; then
+		pcid=$(echo ${BASH_REMATCH[0]} | rev | cut -f1 -d' ' | rev)
+	else
+		rm temp.html
+		exit
+	fi
 fi
+
+# echo $1 $pcid
 
 #
 # If we did find a PubChem ID, try to find the 3D version of the
@@ -46,5 +60,7 @@ wget https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/$pcid/record/SDF/\?r
 if [ $? -ne 0 ]; then
 	rm $2
 fi
+
+rm temp.html
 
 # https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/2733526/record/SDF/?record_type=3d&response_type=save&response_basename=Structure3D_CID_2733526
